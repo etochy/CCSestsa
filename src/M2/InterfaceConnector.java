739acf25e84234ba51613f5e90dc051b8345cfbc
e157572z <sacha.loriot@etu.univ-nantes.fr>
2017-12-09 16:12:34 +0100

    Interf connec corrected

--- src/M2/InterfaceConnector.java
+++ src/M2/InterfaceConnector.java
@@ -1,16 +1,22 @@
 package M2;
 
+import java.util.List;
+
 public abstract class InterfaceConnector {
-	private Role role;
+	private List<Role> role;
 	private String type;
 	
 	// Type : Supplied / Required
 	// TODO : Change for a TYPE with two constants
-	public InterfaceConnector (Role r, String type) {
+	public InterfaceConnector (List<Role> r, String type) {
 		this.role = r;
 	}
 	
 	public String getInterfaceType () {
 		return type;
+	}
+	
+	public void addRole (Role r) {
+		role.add(r);
 	}
 }
