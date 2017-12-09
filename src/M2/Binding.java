927dcdf97957875df6136e0162edb7b1d7bb3ee3
etochy <etochy@gmail.com>
2017-12-09 21:38:25 +0100

    Comm config + Composant

--- src/M2/Binding.java
+++ src/M2/Binding.java
@@ -2,20 +2,31 @@ package M2;
 
 public class Binding extends Lien{
 	
-	private Port port;
+	private Port port1; //port extern, en provenance d'un composant
+	private Port port2; //port intern
 	
-	public Binding(Configuration context, Type t, Port p) {
+	public Binding(Configuration context, Type t, Port p1, Port p2) {
 		super(context,t);
-		this.port = p;
+		this.port1 = p1;
+		this.port2 = p2;
 		// TODO Auto-generated constructor stub
 	}
 
-	public Port getPort() {
-		return port;
+	public Port getPort1() {
+		return port1;
 	}
 
-	public void setPort(Port port) {
-		this.port = port;
+	public void setPort1(Port port1) {
+		this.port1 = port1;
 	}
-	
+
+	public Port getPort2() {
+		return port2;
+	}
+
+	public void setPort2(Port port2) {
+		this.port2 = port2;
+	}
+
+
 }
--- src/M2/Configuration.java
+++ src/M2/Configuration.java
@@ -21,6 +21,60 @@ public class Configuration implements GComposant, GConnector{
 		this.listAttachments = listAttachments;
 	}
 	
+	public void send(String msg, Port p) {
+		String name = p.getName();
+		if (listBindings.size() > 0) {
+			// Parcours bindings puis attachements
+			for(Binding b : listBindings) {
+				if(b.getPort1().getName().equals("name")) {
+					for(Attachment a : listAttachments) {
+						if (a.getPort().equals(b.getPort2().getName())) {
+							a.getRole().receive(msg);
+						}
+					}
+				}
+			}
+		}
+		// parcours des attachements liés a ce port
+		for(Attachment a : listAttachments) {
+			if (a.getPort().equals("name")) {
+				a.getRole().receive(msg);
+			}
+		}
+	}
+	
+	public void send(String msg, Role r) {
+		String name = r.getName();
+		// parcours des attachements liés a ce port
+		for(Attachment a : listAttachments) {
+			if (a.getRole().equals("name")) {
+				a.getPort().receive(msg);
+				for(Binding b : listBindings) {
+					if(b.getPort2().getName().equals(a.getPort().getName())) {
+						b.getPort1().receive(msg);
+					}
+				}
+			}
+		}
+	}
+	
+	public void receive(String msg, Port p) {
+		String name = p.getName();
+		if (listBindings.size() > 0) {
+			// Parcours bindings puis attachements
+			for(Binding b : listBindings) {
+				if(b.getPort1().getName().equals(name)) {
+					b.getPort1().receive(msg);
+				}
+			}
+		}
+	}
+	
+	public void receive(String msg, Role r) {
+		// je sais pas trop comment faire, ni même si le msg va remonter ici
+	}
+	
+	
 	public List<GComposant> getListComposants() {
 		return listComposants;
 	}
